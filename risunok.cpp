8efea87a544ad0ce048f2087e54ce3023135e4ee
ArtSv <ArtSv@DESKTOP-JK44D29>
2024-05-13 10:43:02 +0300

    Добавьте файлы проекта.

---
+++ risunok.cpp
@@ -0,0 +1,23 @@
+﻿// risunok.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Составьте программу, которая напечатает рисунок, используя символы из таблицы ASCII
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int r;
+    char c;
+
+    cout << "Введите код: ";
+    cin >> r;
+
+    c = r;
+    cout << c << endl;
+
+    return 0;
+}
+
